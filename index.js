f5e8b6fd16764c7e79cf3cd89af2739243f3a93b
Bin Tran <cuong.tran@thinknextco.com>
2021-12-08 22:47:31 +0700

    Update 08/12/2021 Part 2

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main");
+    const sliderItem = document.querySelectorAll(".slider-item");
+    const nextBtn = document.querySelector(".slider-next");
+    const nextPrev = document.querySelector(".slider-prev");
+    const dotItem = document.querySelectorAll(".slider-dot-item");
+},false)
